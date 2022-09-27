953a8f1ba775bf06040070526a69664f0f92e8e3
tran0460 <tran0460@algonquinlive.com>
2022-09-27 18:31:21 -0400

    style results list

--- src/components/ResultsList/ResultsList.jsx
+++ src/components/ResultsList/ResultsList.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import styles from "./ResultsList.module.css";
 
 const ResultsList = (props) => {
   const { results } = props;
   if (!results) return;
   return (
-    <div>
+    <div className={styles["list-container"]}>
+      <h2>Search Results</h2>
       {results.map((item) => {
         return <div key={item.id}>{item.login}</div>;
       })}
