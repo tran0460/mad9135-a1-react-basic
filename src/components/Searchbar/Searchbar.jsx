984b2a14dad45c2f9a3855697a71b77a054ac891
tran0460 <tran0460@algonquinlive.com>
2022-09-27 16:05:01 -0400

    search bar styling

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "./Searchbar.module.css";
+import MagnifyingGlass from "../../assets/Icons/MagnifyingGlass";
 
-const Searchbar = () => {
+const Searchbar = (props) => {
+  const { onSearch } = props;
   return (
     <div className={styles["input-container"]}>
       <input
@@ -9,6 +11,9 @@ const Searchbar = () => {
         type="text"
         placeholder="Search bar"
       />
+      <span className={styles["icon-container"]} onClick={() => onSearch()}>
+        <MagnifyingGlass color={"#000"} size={24} />
+      </span>
     </div>
   );
 };
