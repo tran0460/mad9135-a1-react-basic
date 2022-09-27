4c948212162b372a7e0a09e7510ba4589336a385
tran0460 <tran0460@algonquinlive.com>
2022-09-26 20:59:00 -0400

    header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./Header.module.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles["header-container"]}>
+      <h1>Github User Finder</h1>
+    </div>
+  );
 };
 
 export default Header;
