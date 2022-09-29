1a858df68fd8ac38bed8bc3eaf498668d09f5958
tran0460 <tran0460@algonquinlive.com>
2022-09-28 20:15:52 -0400

    styling

--- src/components/ModalContent/ModalContent.jsx
+++ src/components/ModalContent/ModalContent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./ModalContent.module.css";
 import XIcon from "../../assets/Icons/XIcon";
-
+import { GithubCatDark } from "../../logoHelper";
 const ModalContent = (props) => {
   const { data, onRequestClose } = props;
   if (!data) return;
@@ -13,9 +13,23 @@ const ModalContent = (props) => {
       </span>
       <img className={styles["modal-image"]} src={data.avatar_url} alt="" />
       <p className={styles["modal-user-name"]}>{data.name}</p>
-      <p className={styles["modal-bio"]}>{data.bio}</p>
+      <p style={{ display: data.location ? "" : "none" }}>{data.location}</p>
+      <p style={{ display: data.company ? "" : "none" }}>{data.company}</p>
+      <p
+        style={{ display: data.bio ? "" : "none" }}
+        className={[styles["modal-bio"]]}>
+        {data.bio}
+      </p>
       <p className={styles["modal-social-info"]}>Following: {data.following}</p>
       <p className={styles["modal-social-info"]}>Followers: {data.followers}</p>
+      <a
+        className={styles["modal-link"]}
+        href={data.html_url}
+        rel="noreferrer"
+        target="_blank">
+        <GithubCatDark size={20} />
+        View on github
+      </a>
     </div>
   );
 };
